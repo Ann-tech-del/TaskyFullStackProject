8024e3a3cbef329115929c5adbd76418fdd551ca
Ann Granian <annobongo@gmail.com>
2025-07-28 11:27:11 +0300

    added vercel.json file

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ const app :Express= express()
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
-origin:["http://localhost:5173"],
+origin:["https://tasky-full-stack-project.vercel.app/"],
 methods:["PUT","GET","POST","DELETE","PATCH"] ,
 credentials: true,   
 }))
